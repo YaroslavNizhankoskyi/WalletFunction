ba1c24590f3ddcba1a42cd54f5026fda64db2cad
Yaroslav Nizhankovskyi <yaroslav.nizhankovskyi@gmail.com>
2022-07-14 12:29:09 +0300

    Added service Bus reader function

---
+++ WalletFunction/SendEmailFunction.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WalletFunction
+{
+    internal static class SendEmailFunction
+    {
+        [FunctionName("EmailBusTrigger")]
+        public static void Run(
+        [ServiceBusTrigger("email", Connection = "ServiceBusConnectionString")]
+        string email,
+        DateTime enqueuedTimeUtc,
+        string messageId,
+        ILogger log)
+        {
+            log.LogInformation($"Email: {email}");
+            log.LogInformation($"Time: {enqueuedTimeUtc}");
+            log.LogInformation($"MessageId: {messageId}");
+        }
+    }
+}
