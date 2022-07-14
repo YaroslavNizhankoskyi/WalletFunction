df0538951f11ed5d28e96bcfe6d52c205197f385
Yaroslav Nizhankovskyi <yaroslav.nizhankovskyi@gmail.com>
2022-07-14 17:29:08 +0300

    Email sending function

---
+++ WalletFunction/Models/EmailMessageDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WalletFunction.Models
+{
+    internal class EmailMessageDto
+    {
+        public int LeftAmount { get; set; }
+
+        public string UserEmail { get; set; }
+
+        public string WalletName { get; set; }
+    }
+}
--- WalletFunction/SendEmailFunction.cs
+++ WalletFunction/SendEmailFunction.cs
@@ -1,26 +1,49 @@
 ﻿using Microsoft.Azure.WebJobs;
 using Microsoft.Extensions.Logging;
+using MimeKit;
+using MimeKit.Text;
+using MailKit.Net.Smtp;
+using MailKit.Security;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WalletFunction.Models;
 
 namespace WalletFunction
 {
     internal static class SendEmailFunction
     {
         [FunctionName("EmailBusTrigger")]
-        public static void Run(
+        public static async Task Run(
         [ServiceBusTrigger("email", Connection = "ServiceBusConnectionString")]
-        string email,
+        EmailMessageDto emailDto,
         DateTime enqueuedTimeUtc,
         string messageId,
         ILogger log)
         {
-            log.LogInformation($"Email: {email}");
+            log.LogInformation($"User Email: {emailDto.UserEmail}");
             log.LogInformation($"Time: {enqueuedTimeUtc}");
             log.LogInformation($"MessageId: {messageId}");
+
+            var email = new MimeMessage();
+            email.From.Add(MailboxAddress.Parse(Environment.GetEnvironmentVariable("SenderEmail")));
+            email.To.Add(MailboxAddress.Parse(emailDto.UserEmail));
+            email.Subject = "Low budget";
+            email.Body = new TextPart(TextFormat.Plain) 
+            {
+                Text = $"Your wallet {emailDto.WalletName}, has low blance of {emailDto.LeftAmount}" 
+            };
+
+            using var smtp = new SmtpClient();
+            smtp.Connect("smtp.gmail.com", 465, true);
+            smtp.AuthenticationMechanisms.Remove("XOAUTH2");
+            smtp.Authenticate(Environment.GetEnvironmentVariable("SenderEmail"),
+                Environment.GetEnvironmentVariable("SenderPassword"));
+            var response = await smtp.SendAsync(email);
+            smtp.Disconnect(true);
+
         }
     }
 }
