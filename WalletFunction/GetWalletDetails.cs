cc2229e2aa33cc2b0803b67461b20d863176c10e
Yaroslav Nizhankovskyi <yaroslav.nizhankovskyi@gmail.com>
2022-07-25 11:32:59 +0300

    Get wallet details function

---
+++ WalletFunction/GetWalletDetails.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WalletFunction.Models;
+
+namespace WalletFunction
+{
+    internal class GetWalletDetails
+    {
+        [FunctionName("GetWalletDetails")]
+        public static async Task<IActionResult> Run(
+        [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "wallets/{walletId}")] HttpRequest req,
+        [CosmosDB(
+            databaseName: "wallet-niz",
+            collectionName: "wallet",
+            ConnectionStringSetting = "CosmosDbConnectionString",
+            SqlQuery =
+                "SELECT * FROM c " +
+                "WHERE c.id = {walletId} ")]
+        IEnumerable<Wallet> wallets,
+        ILogger log)
+        {
+            return new OkObjectResult(wallets.FirstOrDefault());
+        }
+    }
+}
